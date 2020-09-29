3a8f3069a3875fa391aa568b13356888ee9ecb4e
Edwin Ramírez <eramirez@grupocentrico.com>
2020-09-29 18:50:31 -0500

    Refactor

--- course/course.go
+++ course/course.go
@@ -3,24 +3,48 @@ package course
 import "fmt"
 
 // Course type
-type Course struct {
-	Name    string
-	Price   float64
-	IsFree  bool
-	UserIDs []uint
-	Lessons map[uint]string
+type course struct {
+	name    string
+	price   float64
+	isFree  bool
+	userIDs []uint
+	lessons map[uint]string
+}
+
+func (c *course) Name() string        { return c.name }
+func (c *course) SetName(name string) { c.name = name }
+
+func (c *course) Price() float64         { return c.price }
+func (c *course) SetPrice(price float64) { c.price = price }
+
+func (c *course) IsFree() bool          { return c.isFree }
+func (c *course) SetIsFree(isFree bool) { c.isFree = isFree }
+
+func (c *course) UserIDs() []uint       { return c.userIDs }
+func (c *course) SetUserIDs(IDs []uint) { c.userIDs = IDs }
+
+func (c *course) Lessons() map[uint]string           { return c.lessons }
+func (c *course) SetLessons(lessons map[uint]string) { c.lessons = lessons }
+
+// New course
+func New(name string, price float64, isFree bool) *course {
+	if price == 0 {
+		price = 24.99
+	}
+
+	return &course{
+		name:   name,
+		price:  price,
+		isFree: isFree,
+	}
 }
 
 // PrintLessons of a course
-func (c *Course) PrintLessons() {
+func (c *course) PrintLessons() {
 	text := "The lessons are: "
-	for _, value := range c.Lessons {
+	for _, value := range c.lessons {
 		text += value + ", "
 	}
 
 	fmt.Println(text[:len(text)-2])
-}
-
-func (c *Course) updatePrice(p float64) {
-	c.Price = p
 }
--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/tavo1987/oop/course"
 )
 
 func main() {
+	golang := course.New("Golang from scratch", 25, false)
+	golang.SetUserIDs([]uint{1, 6, 3, 5})
+	golang.SetLessons(map[uint]string{
+		1: "Introduction",
+		2: "Structures",
+		3: "Maps",
+	})
 
-	golang := course.Course{
-		Name:    "Golang from scratch",
-		Price:   29.99,
-		IsFree:  false,
-		UserIDs: []uint{1, 6, 3, 5},
-		Lessons: map[uint]string{
-			1: "Introduction",
-			2: "Structures",
-			3: "Maps",
-		},
-	}
+	golang.SetPrice(65)
 
-	golang.updatePrice(45.99)
-
-	fmt.Println(golang)
+	golang.PrintLessons()
 }
